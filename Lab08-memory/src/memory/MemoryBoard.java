0983177032aae7b5956d8cedaeed8258e94ad6af
a1vv <41792248+a1vv@users.noreply.github.com>
2021-01-28 12:28:14 +0100

    lab 8 finished

--- Lab08-memory/src/memory/MemoryBoard.java
+++ Lab08-memory/src/memory/MemoryBoard.java
@@ -1,50 +1,87 @@
 package memory;
 
+import java.util.Random;
 
 public class MemoryBoard {
+	private int size;
+	private MemoryCardImage[][] board;
+	private boolean[][] bools;
 
 	/** Skapar ett memorybräde med size * size kort. backFileName är filnamnet 
 	    för filen med baksidesbilden. Vektorn frontFileNames innehåller filnamnen 
 	    för frontbilderna. */
 	public MemoryBoard(int size, String backFileName, String[] frontFileNames) {
-
+		this.size = size;
+		board = new MemoryCardImage[size][size];
+		bools = new boolean[size][size];
+		createCards(backFileName, frontFileNames);
 	}
 
 	/* Skapar size * size / 2 st memorykortbilder.
 	   Placerar ut varje kort på två slumpmässiga ställen på spelplanen. */
 	private void createCards(String backFileName, String[] frontFileNames) {
-
+		int i = 0;
+		MemoryCardImage[] cards = new MemoryCardImage[size*size/2];
+		for(String c : frontFileNames) {
+			cards[i] = new MemoryCardImage(c,backFileName);
+			//varje kort ska placeras två gånger
+			placeCard(cards[i]);
+			placeCard(cards[i]);
+			i++;
+			if(i == cards.length) {
+				break;
+			}
+		}
+	}
+	// hittar en plats för kortet
+	private void placeCard(MemoryCardImage card) {
+		Random rnd = new Random();
+		int r = rnd.nextInt(getSize());
+		int c = rnd.nextInt(getSize());
+		if(getCard(r,c)==null) {
+			board[r][c] = card;
+			bools[r][c] = false;
+		} else {
+			placeCard(card);
+		}
 	}
 
 	/** Tar reda på brädets storlek. */
 	public int getSize() {
-		return 0;
+		return size;
 	}
 	
 	/** Hämtar den tvåsidiga bilden av kortet på rad r, kolonn c.
 	    Raderna och kolonnerna numreras från 0 och uppåt. */
 	public MemoryCardImage getCard(int r, int c) {
-		return null;
+		return board[r][c];
 	}
 
 	/** Vänder kortet på rad r, kolonn c. */
 	public void turnCard(int r, int c) {
-
+		bools[r][c] = !bools[r][c];
 	}
 	
 	/** Returnerar true om kortet r, c har framsidan upp. */
 	public boolean frontUp(int r, int c) {
-		return false;
+		return bools[r][c];
 	}
 	
 	/** Returnerar true om det är samma kort på rad r1, kolonn c2 som på rad r2, 
 	    kolonn c2. */
 	public boolean same(int r1, int c1, int r2, int c2) {
-		return false;
+		return board[r1][c1] == board[r2][c2];
 	}
 
 	/** Returnerar true om alla kort har framsidan upp. */
 	public boolean hasWon() {
-		return false;
+		for(int r = 0 ; r < size ; r++) {
+			for(int c = 0 ; c < size ; c++) {
+				if(!bools[r][c]) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}	
 }
--- Lab08-memory/src/memory/MemoryGame.java
+++ Lab08-memory/src/memory/MemoryGame.java
@@ -1,11 +1,76 @@
 package memory;
 
+import java.util.Scanner;
+
 public class MemoryGame {
+	private static MemoryBoard mb;
+	private static MemoryWindow mw;
+	private static Scanner scan;
+	private static String[] fronts;
+	
 	public static void main(String[] args) {
 		String[] frontFileNames = {"can.jpg", "flopsy_mopsy_cottontail.jpg",
 				"friends.jpg", "mother_ladybird.jpg", "mr_mcgregor.jpg",
-				"mrs_rabbit.jpg", "mrs_tittlemouse.jpg", "radishes.jpg" };
+				"mrs_rabbit.jpg", "mrs_tittlemouse.jpg", "radishes.jpg", "radishes.jpg" };
+		scan = new Scanner(System.in);
+		fronts = frontFileNames;
+		newGame();
 		
-		// Fyll i egen kod här
+	}
+	private static void newGame() {
+		mb = new MemoryBoard(4,"back.jpg",fronts);
+		mw = new MemoryWindow(mb);
+		gameLoop();
+	}
+	
+	private static void gameLoop() {
+		mw.drawBoard();
+		int moves = 0;
+		while(true) {
+			System.out.println("Pick two cards.");
+			turnCards();
+			moves++;
+			if(mb.hasWon()) {
+				mw.close();
+				System.out.println("GAME FINISHED WITH " + moves + " MOVES!");
+				System.out.println("Play again? (y/n)");
+				if(scan.next().equals("y")) {
+					newGame();
+				}
+			}
+		}
+		
+	}
+	private static void turnCards() {
+		int r, c;
+		int r1 = -1;
+		int c1 = -1;
+		int i = 0;
+		while(true) {
+			mw.waitForMouseClick();
+			r = mw.getMouseRow();
+			c = mw.getMouseCol();
+			if(!mb.frontUp(r, c)) {
+				turn(r,c);
+				i++;
+			}
+			if(i == 2) {
+				if(!mb.same(r1, c1, r, c)) {
+					mw.delay(500);
+					turn(r1,c1);
+					turn(r,c);
+				} else {
+					System.out.println("Pair found!");
+				}
+				break;
+			}
+			r1 = r;
+			c1 = c;
+		}
+	}
+	private static void turn(int r, int c) {
+		//System.out.println("turning card: " + r + ", " + c );
+		mb.turnCard(r, c);
+		mw.drawCard(r, c);
 	}
 }
